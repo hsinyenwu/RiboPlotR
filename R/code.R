ce781e251a11a6421b1c90603b1216bdd2b87156
Wu <wu@Wus-MacBook-Pro.local>
2019-10-11 12:02:25 -0400

    bug fixed

--- R/code.R
+++ R/code.R
@@ -91,7 +91,7 @@ uorf.structure <- function(uorf_annotation,format="gtf",dataSource="",organism="
 #' \dontrun{
 #' uorf.structure(uorf_annotation="/Volumes/BACKUP/project2/TAIR10.29.gtf",dataSource="Araport",organism="Arabidopsis thaliana")
 #' }
-rna_bam.ribo <- function(Ribo1,Ribo2,RNAseqBam1,RNAseqBam2,RNAlab1="RNA_sample1",RNAlab2="RNA_sample2",RNAseqBamPaired="paired",Ribolab1="Ribo_sample1",Ribolab2="Ribo_sample2",S_NAME1="sample1",S_NAME2="sample2",RNAbackground="#FEFEAE"){
+rna_bam.ribo <- function(Ribo1,Ribo2=NULL,RNAseqBam1,RNAseqBam2=NULL,RNAlab1="RNA_sample1",RNAlab2=NULL,RNAseqBamPaired="paired",Ribolab1="Ribo_sample1",Ribolab2=NULL,S_NAME1="sample1",S_NAME2=NULL,RNAbackground="#FEFEAE"){
   #get path to RNASeq Bam file
   RNAseqBam1 <- RNAseqBam1
   #get ribo-seq all p-site information
